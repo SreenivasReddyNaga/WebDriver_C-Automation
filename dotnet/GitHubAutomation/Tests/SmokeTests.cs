d150ebb979264bf49ee239574109ff4b02d00907
Sreeni <Sreenivasulu2k3@gmail.com>
2017-08-24 16:01:16 +0530

    adding project files

---
+++ dotnet/GitHubAutomation/Tests/SmokeTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace GitHubAutomation
+{
+    [TestFixture]
+    public class SmokeTests
+    {
+        private Steps.Steps steps = new Steps.Steps();
+        private const string USERNAME = "testautomationuser";
+        private const string PASSWORD = "Time4Death!";
+
+        [SetUp]
+        public void Init()
+        {
+            steps.InitBrowser();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            steps.CloseBrowser();
+        }
+
+        [Test]
+        public void OneCanLoginGithub()
+        {
+            steps.LoginGithub(USERNAME, PASSWORD);
+            Assert.True(steps.IsLoggedIn(USERNAME));
+        }
+
+        [Test]
+        public void OneCanCreateProject()
+        {
+            steps.LoginGithub(USERNAME, PASSWORD);
+            Assert.IsTrue(steps.CreateNewRepository("testRepo", "auto-generated test repo"));
+            Assert.IsTrue(steps.CurrentRepositoryIsEmpty());
+        }
+    }
+}
